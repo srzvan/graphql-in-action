6739c599816c9fd6bcaba3150bf638f6a0660e53
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-09-06 14:48:01 +0300

    implement extractPrefixedProps util function

--- api/src/utils.js
+++ api/src/utils.js
@@ -1,3 +1,21 @@
 import crypto from 'crypto';
 
-export const randomString = (bytesSize = 32) => crypto.randomBytes(bytesSize).toString('hex');
+export function randomString(bytesSize = 32) {
+  return crypto.randomBytes(bytesSize).toString('hex');
+}
+
+export function extractPrefixedProps(prefixedObject, prefix) {
+  const regexp = new RegExp(`^${prefix}_(.*)`);
+
+  return Object.entries(prefixedObject).reduce(reducer, {});
+
+  function reducer(unprefixedObject, [key, value]) {
+    const matchesArr = key.match(regexp);
+
+    if (matchesArr) {
+      unprefixedObject[matchesArr[1]] = value;
+    }
+
+    return unprefixedObject;
+  }
+}
