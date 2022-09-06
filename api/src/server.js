6535385496f851957b087b51c563074d52530555
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-09-06 10:11:06 +0300

    add pgPool to graphQL context

--- api/src/server.js
+++ api/src/server.js
@@ -1,22 +1,23 @@
-import cors from "cors";
-import morgan from "morgan";
-import express from "express";
-import bodyParser from "body-parser";
-import { graphqlHTTP } from "express-graphql";
+import cors from 'cors';
+import morgan from 'morgan';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlHTTP } from 'express-graphql';
 
-import { schema } from "./schema";
-import * as config from "./config";
+import { schema } from './schema';
+import * as config from './config';
+import pgClient from './db/pg-client';
 
 async function main() {
   const server = express();
   server.use(cors());
-  server.use(morgan("dev"));
+  server.use(morgan('dev'));
   server.use(bodyParser.urlencoded({ extended: false }));
   server.use(bodyParser.json());
-  server.use("/:fav.ico", (req, res) => res.sendStatus(204));
+  server.use('/:fav.ico', (req, res) => res.sendStatus(204));
 
-  // Example route
-  server.use("/", graphqlHTTP({ schema, graphiql: true }));
+  const { pgPool } = await pgClient();
+  server.use('/', graphqlHTTP({ schema, context: { pgPool }, graphiql: true }));
 
   // This line runs the server
   server.listen(config.port, () => {
