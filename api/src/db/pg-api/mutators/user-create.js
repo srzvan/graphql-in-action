662cbad6ed7a562161c4ef0c0416553b929b1580
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-10-26 09:46:53 +0300

    pg-api/mutators/user-create - add input validation

--- api/src/db/pg-api/mutators/user-create.js
+++ api/src/db/pg-api/mutators/user-create.js
@@ -2,26 +2,32 @@ import { randomString } from '../../../utils';
 
 export function userCreate(query) {
   return async ({ input }) => {
-    const payload = { errors: [] };
-
-    if (input.password.length < 6) {
-      payload.errors.push({ message: ERRORS.minLength });
-    }
+    const payload = {
+      errors: validateInput(input),
+    };
 
     if (payload.errors.length === 0) {
       const authToken = randomString();
-      const response = await query(statement, {
-        $1: input.username.toLowerCase(),
-        $2: input.password,
-        $3: input.firstName,
-        $4: input.lastName,
-        $5: authToken,
-      });
-      const user = response.rows[0];
 
-      if (user) {
-        payload.user = user;
-        payload.authToken = authToken;
+      try {
+        const response = await query(statement, {
+          $1: input.username.toLowerCase(),
+          $2: input.password,
+          $3: input.firstName,
+          $4: input.lastName,
+          $5: authToken,
+        });
+
+        const user = response.rows[0];
+
+        if (user) {
+          payload.user = user;
+          payload.authToken = authToken;
+        }
+      } catch (error) {
+        payload.errors.push({ message: error.message });
+
+        console.log(error);
       }
     }
 
@@ -29,12 +35,33 @@ export function userCreate(query) {
   };
 }
 
-const ERRORS = {
-  minLength: 'The password must have more than 6 characters',
-};
-
 const statement = `
   INSERT INTO azdev.users (username, hashed_password, first_name, last_name, hashed_auth_token)
   VALUES ($1, crypt($2, gen_salt('bf')), $3, $4, crypt($5, gen_salt('bf')))
   RETURNING id, username, first_name AS "firstName", last_name AS "lastName", created_at AS "createdAt"
 `;
+
+const ERRORS = {
+  minLength: 'The password must have more than 6 characters',
+  onlyEnglishLetters:
+    'First and last names must contain ONLY letters from the English alphabet',
+};
+const onlyEnglishLettersRegex = new RegExp(/^[^\d]*[a-z]+$/, 'i');
+
+function validateInput({ password, firstName, lastName }) {
+  const errors = [];
+
+  if (password.length < 6) {
+    errors.push({ message: ERRORS.minLength });
+  }
+
+  if (firstName && !onlyEnglishLettersRegex.test(firstName)) {
+    errors.push({ message: ERRORS.onlyEnglishLetters });
+  }
+
+  if (lastName && !onlyEnglishLettersRegex.test(lastName)) {
+    errors.push({ message: ERRORS.onlyEnglishLetters });
+  }
+
+  return errors;
+}
